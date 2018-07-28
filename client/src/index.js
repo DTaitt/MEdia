e902caf0d8c345ef2548abc5765a0437c26ce284
Dtaitt <Donovan.Taitt@yahoo.com>
2018-07-27 20:54:30 -0500

    1.3 | setup eslint and relative import

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from 'components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
+	<App />, 
+	document.querySelector('#root')
 );
 
 registerServiceWorker();
