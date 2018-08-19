3f37ea2a896a1569e5e3362347a3d286ce36e16d
Dtaitt <Donovan.Taitt@yahoo.com>
2018-08-18 20:08:19 -0400

    3.1 | updated mediaitem to allow for different names between tv and film

--- client/src/components/MediaItem/MediaItem.jsx
+++ client/src/components/MediaItem/MediaItem.jsx
@@ -5,26 +5,31 @@ import {createMediaUrl, createMediaImageUrl} from 'utilities/utilities';
 
 export default function MediaItem({item}){
 	const {
-		title,
+		title = null,
+		name = null,
 		id,
 		poster_path,
 		vote_average,
-		release_date
+		release_date = null,
+		first_air_date = null
 	} = item;
 
-	const mediaUrl = createMediaUrl(title, id);
+	const mediaTitle = title || name;
+	const releaseDate = release_date || first_air_date;
+
+	const mediaUrl = createMediaUrl(mediaTitle, id);
 	const mediaImageUrl = createMediaImageUrl(poster_path);
 
 	return (
 		<List.Item>
 			<Card 
-				title={<Link to={mediaUrl}>{title}</Link>}
-				cover={<img alt={title} src={mediaImageUrl} />}
+				title={<Link to={mediaUrl}>{mediaTitle}</Link>}
+				cover={<img alt={mediaTitle} src={mediaImageUrl} />}
 				extra={vote_average}
 			>
 				<Card.Meta
 					title={<Link to={mediaUrl}>See More...</Link>}
-					description={release_date}
+					description={releaseDate}
 				/>
 			</Card>
 		</List.Item>
